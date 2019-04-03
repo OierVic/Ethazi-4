9c0c4eb8917744b4ba05d3d89a28e6542014c849
Oier Vicente <oier.vicentefi@elorrieta-errekamari.com>
2019-04-03 08:55:10 +0200

    Klaseak sortu

---
+++ src/Controlador/Aplikazioa.java
@@ -0,0 +1,10 @@
+package Controlador;
+
+public class Aplikazioa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Vistas/Frame.java
@@ -0,0 +1,23 @@
+package Vistas;
+
+import java.awt.GridBagConstraints;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+
+
+	private static final long serialVersionUID = 1L;
+	GridBagConstraints config = new GridBagConstraints();
+
+	public Frame() {
+		this.setTitle("Txartel Saltzailea");
+		
+		this.setSize(770,670);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		
+
+	}
+
+}
