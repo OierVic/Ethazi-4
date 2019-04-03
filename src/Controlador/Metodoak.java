1e82c75399682890014678af15065ed68f94324b
Oier Vicente <oier.vicentefi@elorrieta-errekamari.com>
2019-04-03 10:32:26 +0200

    commit

--- src/Controlador/Metodoak.java
+++ src/Controlador/Metodoak.java
@@ -2,16 +2,21 @@ package Controlador;
 
 import Modelo.Conexion;
 import Vistas.Ventana1;
+import Vistas.Ventana2;
 
 public class Metodoak {
 	
 	public static void lehenengoLeihoa() {
 		
-//		String baseDatos="ethazi4";
-//		Conexion b1 = new Conexion(baseDatos);
-		
 		Ventana1 ventana1= new Ventana1();
 		ventana1.setVisible(true);
+
+	}
+	public static void bigarrenLeihoa() {
+
+		Ventana2 ventana2= new Ventana2();
+		ventana2.setVisible(true);
+
 
 	}
 	
