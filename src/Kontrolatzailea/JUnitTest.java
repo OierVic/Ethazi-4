ad160d7b76a0cbd3e41fa832823efbe8fd9f6638
Oier Vicente <oier.vicentefi@elorrieta-errekamari.com>
2019-05-21 17:25:58 +0200

    JUnit

--- src/Kontrolatzailea/JUnitTest.java
+++ src/Kontrolatzailea/JUnitTest.java
@@ -2,6 +2,8 @@ package Kontrolatzailea;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 
 class JUnitTest {
@@ -12,22 +14,7 @@ class JUnitTest {
 		String pasahitza="abracadabra";
 		assertEquals(Metodoak.ateraMD5(pasahitza),("ec5287c45f0e70ec22d52e8bcbeeb640"));
 	}
-//	@Test
-//	public void  konprobatuDistanciaCoord() {
-//		 double lng1=-2.94974;
-//		 double lat1=43.2614;
-//		 double lng2=-2.99149;
-//		 double lat2=43.3191;
-//		assertEquals(Metodoak.distanciaCoord(lng1, lat1, lng2, lat2),7.251413598425656);
-//	}
-//	@Test
-//	public void prezioaKalkulatu() {
-//		double distantzia=7.251413598425656;
-//		double kontsumo_bus=0.32;
-//		int bidaiariak=40;
-//		assertEquals(Metodoak.prezioaKalkulatu(distantzia, kontsumo_bus, bidaiariak), 0.05569085643590904);
-//		
-//	}
+
 	@Test
 	public void konprobatuNegatibo() {
 		assertEquals(Metodoak.konprobatuNegatibo(2),(true));
